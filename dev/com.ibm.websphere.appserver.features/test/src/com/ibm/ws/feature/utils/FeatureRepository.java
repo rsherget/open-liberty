c66e2d1ea36554b9e897e4523b0e33efe4493be8
Thomas Bitonti <bitonti@us.ibm.com>
2023-09-21 13:42:41 -0400

    First pass rewrite of feature tests.

---
+++ dev/com.ibm.websphere.appserver.features/test/src/com/ibm/ws/feature/utils/FeatureRepository.java
@@ -0,0 +1,221 @@
+/*******************************************************************************
+ * Copyright (c) 2023 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package com.ibm.ws.feature.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class FeatureRepository {
+
+    public static FeatureRepository readFeatures(File root) throws IOException {
+        FeatureFiles featureFiles = new FeatureFiles(root);
+
+        Map<String, FeatureInfo> features = new LinkedHashMap<>(featureFiles.getAllChildren().size());
+
+        for (File featureFile : featureFiles.getAllChildren()) {
+            FeatureInfo feature = new FeatureInfo(featureFile);
+            features.put(feature.getName(), feature);
+        }
+
+        features.values().forEach((FeatureInfo feature) -> {
+            feature.getAutoFeatures().forEach((String autoFeatureName) -> {
+                FeatureInfo autoFeature = features.get(autoFeatureName);
+                if (autoFeature != null) {
+                    autoFeature.addActivatingAutoFeature(autoFeatureName);
+                }
+            });
+        });
+
+        return new FeatureRepository(featureFiles, features);
+    }
+
+    //
+
+    public FeatureRepository(FeatureFiles featureFiles, Map<String, FeatureInfo> features) {
+        this.featureFiles = featureFiles;
+        this.features = features;
+
+        this.visibilityPartitions = this.computeVisibilityPartitions();
+        this.baseVisibilities = this.computeBaseVisibilities();
+    }
+
+    //
+
+    private final FeatureFiles featureFiles;
+
+    public FeatureFiles getFeatureFiles() {
+        return featureFiles;
+    }
+
+    //
+
+    private final Map<String, FeatureInfo> features;
+
+    public Map<String, FeatureInfo> getFeatures() {
+        return features;
+    }
+
+    public int getNumFeatures() {
+        return getFeatures().size();
+    }
+
+    public FeatureInfo getFeature(String name) {
+        return getFeatures().get(name);
+    }
+
+    public void forEach(BiConsumer<? super String, ? super FeatureInfo> action) {
+        getFeatures().forEach(action);
+    }
+
+    public void forEach(Consumer<? super FeatureInfo> action) {
+        getFeatures().values().forEach(action);
+    }
+
+    /**
+     * Display the edition and product kind of a feature.
+     *
+     * Display "not found" text if the feature is not found.
+     *
+     * @param feature The name of a feature.
+     *
+     * @return A print string containing the feature name,
+     *         the feature edition, and the feature product kind.
+     */
+    public String printFeature(String feature) {
+        FeatureInfo featureInfo = getFeature(feature);
+        if (featureInfo == null) {
+            return "Feature: [ *** NOT FOUND: " + feature + " *** ]";
+        } else {
+            return featureInfo.printFeature();
+        }
+    }
+
+    //
+
+    // featureName -> visibility -> feature info
+
+    private final Map<String, Map<String, List<FeatureInfo>>> visibilityPartitions;
+
+    public Map<String, Map<String, List<FeatureInfo>>> getVisibilityPartitions() {
+        return visibilityPartitions;
+    }
+
+    protected Map<String, Map<String, List<FeatureInfo>>> computeVisibilityPartitions() {
+        Map<String, Map<String, List<FeatureInfo>>> usePartitions = new HashMap<>();
+
+        forEach((FeatureInfo featureInfo) -> {
+            if (featureInfo.isAutoFeature()) {
+                return;
+            }
+
+            String feature = featureInfo.getName();
+
+            // javaeePlatform and appSecurity are special because they have dependencies on each other.
+            if (feature.startsWith("io.openliberty.jakartaeePlatform") ||
+                feature.startsWith("com.ibm.websphere.appserver.javaeePlatform") ||
+                feature.startsWith("com.ibm.websphere.appserver.appSecurity")) {
+                return;
+            }
+
+            String baseName = featureInfo.getBaseName();
+            String visibility = featureInfo.getVisibility();
+
+            Map<String, List<FeatureInfo>> partition = usePartitions.computeIfAbsent(baseName, (String useName) -> new HashMap<>());
+            List<FeatureInfo> partitionElement = partition.computeIfAbsent(visibility, (String useVisibility) -> new ArrayList<>());
+
+            partitionElement.add(featureInfo);
+        });
+
+        return usePartitions;
+    }
+
+    private final Map<String, String> baseVisibilities;
+
+    public Map<String, String> getBaseVisibilities() {
+        return baseVisibilities;
+    }
+
+    /**
+     * Build and return a table mapping base feature names to visibilities.
+     *
+     * Ignore auto-features and ignore features which do not have version.
+     *
+     * @return The visibility table.
+     */
+    protected Map<String, String> computeBaseVisibilities() {
+        Map<String, String> useBaseVisibilities = new HashMap<>(getNumFeatures());
+
+        forEach((FeatureInfo featureInfo) -> {
+            if (featureInfo.isAutoFeature()) {
+                return;
+            }
+            if (featureInfo.getVersion() == null) {
+                return;
+            }
+            useBaseVisibilities.put(featureInfo.getBaseName(), featureInfo.getVisibility());
+        });
+
+        return useBaseVisibilities;
+    }
+
+    //
+
+    public static <K, V> Map<K, V> merge(Map<K, V> m1, Map<K, V> m2) {
+        if ((m1 == null) || m1.isEmpty()) {
+            if ((m2 == null) || m2.isEmpty()) {
+                return Collections.emptyMap();
+            } else {
+                return m2;
+            }
+
+        } else if ((m2 == null) || m2.isEmpty()) {
+            return m1;
+
+        } else {
+            Map<K, V> merged = new HashMap<K, V>(m1.size() + m2.size());
+            merged.putAll(m1);
+            merged.putAll(m2);
+            return merged;
+        }
+    }
+
+    public static <E> Set<E> merge(Set<E> s1, Set<E> s2) {
+        if ((s1 == null) || s1.isEmpty()) {
+            if ((s2 == null) || s2.isEmpty()) {
+                return Collections.emptySet();
+            } else {
+                return s2;
+            }
+
+        } else if ((s2 == null) || s2.isEmpty()) {
+            return s1;
+
+        } else {
+            Set<E> merged = new HashSet<E>(s1.size() + s2.size());
+            merged.addAll(s1);
+            merged.addAll(s2);
+            return merged;
+        }
+    }
+}
